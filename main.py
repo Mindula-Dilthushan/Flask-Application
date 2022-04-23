db53ba1c7c1646445c38cfc3219ba911e38ddafb
Mindula Dilthushan Manamperi <minduladilthushan1@gmail.com>
2022-04-23 14:19:40 +0530

    start-server

---
+++ main.py
@@ -0,0 +1,15 @@
+# Mindula Dilthushan
+# minduladilthushan1@gmail.com
+# WEB - Application
+# 22 - 04 -23
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello Flask...!"
+
+if __name__ == "__main__":
+    app.run()
