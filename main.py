f8c84e349d1772b5d26d2565828f51c9f7120db5
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2022-04-30 14:42:36 +0530

    app-scope

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app_scope = "TEST APP Scope"
+
 @app.route("/")
 def index():
+    request_scope = "TEST REQ SCOPE"
+    print("Request App :", request_scope)
     print("Hello Flask Application...!")
     return "Hello Flask Application...!"
+
+print("App Scope --> ", app_scope)
 
 if __name__ == "__main__":
     app.run(debug=True)
